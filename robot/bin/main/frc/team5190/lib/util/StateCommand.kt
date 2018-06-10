a223cd7b4e140e700175ccb1b0fcfda887702c96
Prateek M <prateek.machiraju@outlook.com>
2018-06-10 14:46:49 -0400

    Clean up structure

--- robot/bin/main/frc/team5190/lib/util/StateCommand.kt
+++
@@ -1,7 +0,0 @@
-package frc.team5190.lib.util
-
-import edu.wpi.first.wpilibj.command.Command
-
-class StateCommand(private val condition: () -> Boolean) : Command() {
-    override fun isFinished() = condition.invoke()
-}
