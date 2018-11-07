00c17472d62f60711e0dbda42861bc4f4890431f
Ryan Segerstrom <13050203+ThePrimedTNT@users.noreply.github.com>
2018-11-07 07:53:47 -0500

    Remove Characterization command and make manual drive command use methods in TankDrive

--- src/main/kotlin/org/ghrobotics/robot/subsytems/drive/ManualDriveCommand.kt
+++ src/main/kotlin/org/ghrobotics/robot/subsytems/drive/ManualDriveCommand.kt
@@ -5,9 +5,7 @@
 
 package org.ghrobotics.robot.subsytems.drive
 
-import com.ctre.phoenix.motorcontrol.ControlMode
 import edu.wpi.first.wpilibj.GenericHID
-import edu.wpi.first.wpilibj.drive.DifferentialDrive
 import org.ghrobotics.lib.commands.FalconCommand
 import org.ghrobotics.lib.utils.withDeadband
 import org.ghrobotics.lib.wrappers.hid.getRawButton
@@ -19,66 +17,17 @@ import org.ghrobotics.robot.Controls
 class ManualDriveCommand : FalconCommand(DriveSubsystem) {
 
     companion object {
-        private var deadband = 0.02
+        private const val deadband = 0.02
         private val speedSource = Controls.mainXbox.getY(GenericHID.Hand.kLeft).withDeadband(deadband)
         private val rotationSource = Controls.mainXbox.getX(GenericHID.Hand.kLeft).withDeadband(deadband)
         private val quickTurnSource = Controls.mainXbox.getRawButton(kX)
     }
 
-    private var stopThreshold = DifferentialDrive.kDefaultQuickStopThreshold
-    private var stopAlpha = DifferentialDrive.kDefaultQuickStopAlpha
-    private var stopAccumulator = 0.0
-
     override suspend fun execute() {
-        val speed = -speedSource()
-        val rotation = rotationSource()
-
-        val angularPower: Double
-        val overPower: Boolean
-
-        if (quickTurnSource()) {
-            if (Math.abs(speed) < stopThreshold) {
-                stopAccumulator = (1 - stopAlpha) * stopAccumulator + stopAlpha * rotation.coerceIn(-1.0, 1.0) * 2.0
-            }
-            overPower = true
-            angularPower = rotation
-        } else {
-            overPower = false
-            angularPower = Math.abs(speed) * rotation - stopAccumulator
-
-            when {
-                stopAccumulator > 1 -> stopAccumulator -= 1.0
-                stopAccumulator < -1 -> stopAccumulator += 1.0
-                else -> stopAccumulator = 0.0
-            }
-        }
-
-        var leftMotorOutput = speed + angularPower
-        var rightMotorOutput = speed - angularPower
-
-        // If rotationVector is overpowered, reduce both outputs to within acceptable range
-        if (overPower) {
-            when {
-                leftMotorOutput > 1.0 -> {
-                    rightMotorOutput -= leftMotorOutput - 1.0
-                    leftMotorOutput = 1.0
-                }
-                rightMotorOutput > 1.0 -> {
-                    leftMotorOutput -= rightMotorOutput - 1.0
-                    rightMotorOutput = 1.0
-                }
-                leftMotorOutput < -1.0 -> {
-                    rightMotorOutput -= leftMotorOutput + 1.0
-                    leftMotorOutput = -1.0
-                }
-                rightMotorOutput < -1.0 -> {
-
-                    leftMotorOutput -= rightMotorOutput + 1.0
-                    rightMotorOutput = -1.0
-                }
-            }
-        }
-
-        DriveSubsystem.set(ControlMode.PercentOutput, leftMotorOutput, rightMotorOutput)
+        DriveSubsystem.curvatureDrive(
+            -speedSource(),
+            rotationSource(),
+            quickTurnSource()
+        )
     }
 }
